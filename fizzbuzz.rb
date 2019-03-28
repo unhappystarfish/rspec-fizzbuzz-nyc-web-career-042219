ff61141fbd815590b6e4bf52e3528d2c4ca53a58
Jake L <jakelovitz@gmail.com>
2019-03-28 17:37:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num % == 0
+  if num % 15 == 0
     "fizzbuzz"
-  elsif num % == 
+  elsif num % 3 == 0
+    "fizz"
+  else
+    NIL
+  end
 end
